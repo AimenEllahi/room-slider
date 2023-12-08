7f4fe0c13cce3748de6c82e9e178e17fb633b528
hananmunir <hananmunir471@gmail.com>
2023-12-09 01:07:25 +0500

    Pushign CHanges

--- src/Components/Scene.jsx
+++ src/Components/Scene.jsx
@@ -7,15 +7,18 @@ import { Model } from "./Room";
 import Room from "./Room2";
 import Slider from "./Slider";
 import { gsap } from "gsap";
+import SplitType from "split-type";
 
 const models = [
   {
-    model: <Model key="model1" />,
-    text: "Description for Model",
+    heading: "Heading 1",
+    model: <Model key='model1' />,
+    text: "This is Description for model 1. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
   },
   {
-    model: <Room key="model2" />,
-    text: "Description for Model",
+    heading: "Heading 2",
+    model: <Room key='model2' />,
+    text: "This is Description for model 2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
   },
 ];
 
@@ -23,61 +26,99 @@ export default function Scene() {
   const [currentModelIndex, setCurrentModelIndex] = useState(0);
   const ref = useRef();
   const textRef = useRef();
+  const headerRef = useRef();
+
+  const headerOutAnimation = () => {
+    const ourText = new SplitType(headerRef.current, { types: "chars" });
+    const chars = ourText.chars;
+    gsap.fromTo(
+      chars,
+      {
+        x: 0,
+        opacity: 1,
+      },
+      {
+        x: -100,
+        opacity: 0,
+        stagger: 0.05,
+        duration: 2,
+        ease: "power4.out",
+      }
+    );
+  };
+  const headerInAnimation = () => {
+    const ourText = new SplitType(headerRef.current, { types: "chars" });
+    const chars = ourText.chars;
+    gsap.fromTo(
+      chars,
+      {
+        x: 100,
+        opacity: 0,
+      },
+      {
+        x: 0,
+        opacity: 1,
+        stagger: 0.05,
+        duration: 2,
+        ease: "power4.out",
+        delay: 2,
+      }
+    );
+  };
 
   const handleNextModel = () => {
     gsap.to(ref.current.position, {
-      duration: 2,
-      x: -window.innerWidth / 2,
-      y: -window.innerHeight / 2,
+      duration: 1,
+      x: -15,
+      y: -3,
       z: 0,
-      ease: "power3.inOut",
-      onStart: () => {
-        gsap.to(textRef.current, { duration: 2, opacity: 0 });
-      },
+      ease: "power4.inOut",
+      onStart: headerOutAnimation,
       onComplete: () => {
         setCurrentModelIndex((prevIndex) => (prevIndex + 1) % models.length);
+
         gsap.fromTo(
           ref.current.position,
           {
-            x: window.innerWidth / 2,
-            y: -window.innerHeight / 2,
+            x: 15,
+            y: -3,
             z: 0,
           },
           {
-            duration: 2,
+            duration: 1,
             x: 0,
             y: -0.7,
             z: 0,
-            ease: "power3.inOut",
+            ease: "power4.inOut",
             onStart: () => {
-              // Rotate the model continuously
-              gsap.to(ref.current.rotation, {
-                duration: 2,
-                y: "+=6.28319", // This is equivalent to Math.PI * 2
-                ease: "none",
-
-                onUpdate: () => {
-                  // Any additional logic you want to run on each update
+              gsap.fromTo(
+                ref.current.rotation,
+                {
+                  y: -1.5,
                 },
-              });
+                {
+                  duration: 1.5,
+                  y: 0,
+                  ease: "power4.inOut",
+                }
+              );
             },
           }
         );
-        gsap.to(textRef.current, { duration: 2, opacity: 1, delay: 2 });
+
+        headerInAnimation();
       },
     });
   };
 
   const handlePrevModel = () => {
     gsap.to(ref.current.position, {
-      duration: 2,
-      x: window.innerWidth / 2,
-      y: -window.innerHeight / 2,
+      duration: 1,
+      x: 15,
+      y: -3,
       z: 0,
-      ease: "power3.inOut",
-      onStart: () => {
-        gsap.to(textRef.current, { duration: 2, opacity: 0 });
-      },
+      ease: "power4.inOut",
+      onStart: headerOutAnimation,
       onComplete: () => {
         setCurrentModelIndex((prevIndex) =>
           prevIndex === 0 ? models.length - 1 : prevIndex - 1
@@ -86,90 +127,78 @@ export default function Scene() {
         gsap.fromTo(
           ref.current.position,
           {
-            x: -window.innerWidth / 2,
-            y: -window.innerHeight / 2,
+            x: -15,
+            y: -3,
             z: 0,
           },
           {
-            duration: 2,
+            duration: 1,
             x: 0,
             y: -0.7,
             z: 0,
-            ease: "power3.inOut",
+            ease: "power4.inOut",
             onStart: () => {
               // Rotate the model continuously
-              gsap.to(ref.current.rotation, {
-                duration: 2,
-                y: "+=6.28319",
-                ease: "none",
-
-                onUpdate: () => {
-                  // Any additional logic you want to run on each update
+              gsap.fromTo(
+                ref.current.rotation,
+                {
+                  y: 1.5,
                 },
-              });
+                {
+                  duration: 1.5,
+                  y: 0,
+                  ease: "power4.inOut",
+                }
+              );
             },
           }
         );
-        gsap.to(textRef.current, { duration: 2, opacity: 1, delay: 2 });
+
+        headerInAnimation();
       },
     });
   };
 
   return (
-    <div>
-      <div className="w-screen h-screen">
+    <div className='relative'>
+      <div className='w-screen h-screen'>
         <Canvas shadowMap camera={{ position: [0, 2, 10], fov: 50 }}>
-          <color attach="background" args={["#E1C78F"]} />
-          <ambientLight intensity={2} color={"orange"} />
-          <directionalLight
-            castShadow
-            intensity={10}
-            position={[0, 10, 0]}
-            shadow-mapSize-width={1024}
-            shadow-mapSize-height={1024}
-            shadow-camera-far={50}
-            shadow-camera-left={-10}
-            shadow-camera-right={10}
-            shadow-camera-top={10}
-            shadow-camera-bottom={-10}
-            color={"orange"}
-          />
+          <color attach='background' args={["#E1C78F"]} />
+          <ambientLight intensity={0.5} />
+          <directionalLight castShadow intensity={1} position={[0, 10, 0]} />
+          <directionalLight castShadow intensity={1} position={[10, 10, 5]} />
+          <Environment preset='apartment' />
           <Suspense fallback={<Loader />}>
-            <Environment preset="sunset" />
             <group position={[0, -0.7, 0]} ref={ref}>
               {models[currentModelIndex].model}
             </group>
-            <group position={[0, -4.4, 4]}>
-              <Html
-                position={[0, 3, 0]}
-                transform
-                sprite
-                occlude
-                distanceFactor={10}
-              >
-                <div
-                  className="text-[#7A3E3E] flex flex-col justify-center items-center text-center text-2xl font-bold"
-                  ref={textRef}
-                >
-                  {models[currentModelIndex].text}
-
-                  <button className="bg-white text-[#7A3E3E] rounded-lg px-4 py-0.5 text-xs hover:outline-none hover:border-none focus:border-none focus:outline-none hover:cursor-pointer hover:bg-[#7A3E3E] hover:text-white transition-all duration-300">
-                    Buy
-                  </button>
-                </div>
-              </Html>
-            </group>
           </Suspense>
           <ContactShadows
-            rotation-x={Math.PI / 2}
-            position={[-0.3, -2, 0.4]}
-            opacity={0.5}
-            width={30}
-            height={30}
+            position={[0, -1, 0]}
+            opacity={1}
             blur={1}
             far={4.4}
           />
         </Canvas>
+      </div>
+      <div className=' absolute top-14 gapy-y-4 left-[50%] text-4xl translate-x-[-50%] flex flex-col justify-center items-center text-center '>
+        <div
+          ref={headerRef}
+          className='text-[#7A3E3E]   font-semibold text-animation'
+        >
+          {models[currentModelIndex].heading}
+        </div>
+      </div>
+      <div
+        ref={textRef}
+        className=' absolute bottom-10 gapy-y-4 left-[50%] translate-x-[-50%] flex flex-col justify-center items-center text-center '
+      >
+        <div className='text-[#7A3E3E] font-medium  '>
+          {models[currentModelIndex].text}
+        </div>
+        <button className='bg-white text-[#7A3E3E] my-4 rounded-lg px-8 py-2  hover:outline-none hover:border-transparent focus:border-none focus:outline-none hover:cursor-pointer hover:bg-[#7A3E3E] hover:text-white transition-all duration-300'>
+          Buy
+        </button>
       </div>
 
       <Slider onNext={handleNextModel} onPrev={handlePrevModel} />
