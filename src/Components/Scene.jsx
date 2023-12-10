409f59ace4f318cfeea7f74d2b6f7241f7ce62a9
Aimen Elahi <aimenqaiser2000@gmail.com>
2023-12-10 01:38:10 -0800

    added bg

--- src/Components/Scene.jsx
+++ src/Components/Scene.jsx
@@ -8,16 +8,17 @@ import Room from "./Room2";
 import Slider from "./Slider";
 import { gsap } from "gsap";
 import SplitType from "split-type";
+import { Background } from "./Background";
 
 const models = [
   {
     heading: "Heading 1",
-    model: <Model key='model1' />,
+    model: <Model key="model1" />,
     text: "This is Description for model 1. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
   },
   {
     heading: "Heading 2",
-    model: <Room key='model2' />,
+    model: <Room key="model2" />,
     text: "This is Description for model 2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
   },
 ];
@@ -160,14 +161,15 @@ export default function Scene() {
   };
 
   return (
-    <div className='relative'>
-      <div className='w-screen h-screen'>
+    <div className="relative">
+      <div className="w-screen h-screen">
         <Canvas shadowMap camera={{ position: [0, 2, 10], fov: 50 }}>
-          <color attach='background' args={["#E1C78F"]} />
+          {/* <color attach="background" args={["#E1C78F"]} /> */}
+          <Background />
           <ambientLight intensity={0.5} />
           <directionalLight castShadow intensity={1} position={[0, 10, 0]} />
           <directionalLight castShadow intensity={1} position={[10, 10, 5]} />
-          <Environment preset='apartment' />
+          <Environment preset="apartment" />
           <Suspense fallback={<Loader />}>
             <group position={[0, -0.7, 0]} ref={ref}>
               {models[currentModelIndex].model}
@@ -181,22 +183,22 @@ export default function Scene() {
           />
         </Canvas>
       </div>
-      <div className=' absolute top-14 gapy-y-4 left-[50%] text-4xl translate-x-[-50%] flex flex-col justify-center items-center text-center '>
+      <div className=" absolute top-14 gapy-y-4 left-[50%] text-4xl translate-x-[-50%] flex flex-col justify-center items-center text-center ">
         <div
           ref={headerRef}
-          className='text-[#7A3E3E]   font-semibold text-animation'
+          className="text-[#7A3E3E]   font-semibold text-animation"
         >
           {models[currentModelIndex].heading}
         </div>
       </div>
       <div
         ref={textRef}
-        className=' absolute bottom-10 gapy-y-4 left-[50%] translate-x-[-50%] flex flex-col justify-center items-center text-center '
+        className=" absolute bottom-10 gapy-y-4 left-[50%] translate-x-[-50%] flex flex-col justify-center items-center text-center "
       >
-        <div className='text-[#7A3E3E] font-medium  '>
+        <div className="text-[#7A3E3E] font-medium  ">
           {models[currentModelIndex].text}
         </div>
-        <button className='bg-white text-[#7A3E3E] my-4 rounded-lg px-8 py-2  hover:outline-none hover:border-transparent focus:border-none focus:outline-none hover:cursor-pointer hover:bg-[#7A3E3E] hover:text-white transition-all duration-300'>
+        <button className="bg-white text-[#7A3E3E] my-4 rounded-lg px-8 py-2  hover:outline-none hover:border-transparent focus:border-none focus:outline-none hover:cursor-pointer hover:bg-[#7A3E3E] hover:text-white transition-all duration-300">
           Buy
         </button>
       </div>
